dc9bb01bbf1f82a5a1f50cf82c4b8405cd87332f
mmosc <ma.moscati@gmail.com>
2024-01-15 11:51:58 +0100

    update correlation.ipynb

--- utils.py
+++ utils.py
@@ -25,3 +25,38 @@ def compute_cohen_on_pandas_columns(onion_data: pd.DataFrame, emma_data: pd.Data
         emma_data = np.concatenate(emma_data.to_numpy())
     kappa = cohen_kappa_score(onion_data, emma_data)
     return kappa
+
+
+def compute_contingency_table(onion_data: pd.DataFrame, emma_data: pd.DataFrame, gems: Union[str, List[str]]):
+    """
+    Computes the contingency table over two dataframes storing binary data, over either a column (if gems is a string)
+    or over several (if gems is a list of strings).
+
+    :param onion_data:
+    :param emma_data:
+    :param gems:
+    :return contingency: Contingency table as numpy array
+    """
+
+    pairs = list(zip(onion_data[gems].to_numpy().ravel(), emma_data[gems].to_numpy().ravel()))
+
+    n_00 = 0
+    n_01 = 0
+    n_10 = 0
+    n_11 = 0
+    for tuple in pairs:
+        if tuple == (0, 0):
+            n_00 += 1
+        elif tuple == (0, 1):
+            n_01 += 1
+        elif tuple == (1, 0):
+            n_10 += 1
+        elif tuple == (1, 1):
+            n_11 += 1
+
+    contingency = np.array([
+        [n_00, n_01],
+        [n_10, n_11]
+    ])
+
+    return contingency
